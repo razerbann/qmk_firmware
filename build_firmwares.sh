c8ba7555eb910cc1510f4418547d1c321bb148d3
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2020-03-26 22:40:22 +0100

    Added some fancy formatting

--- build_firmwares.sh
+++ build_firmwares.sh
@@ -1,90 +1,113 @@
 #!/bin/bash
 
-git fetch origin
-git fetch upstream
-
 # Update master branch from qmk/qmk_firware base repository
 echo "############################################"
 echo "#### Updating code from base repository ####"
 echo "############################################"
-git rebase upstream/master
-git push origin master -f
+if ! git fetch origin 1>/dev/null;
+then
+  exit 1
+fi
+if ! git fetch upstream 1>/dev/null;
+then
+  exit 2
+fi
+if ! git rebase upstream/master 1>/dev/null;
+then
+  exit 1
+fi
+if ! git push origin master -f 1>/dev/null;
+then
+  exit 2
+fi
+echo
 
 # Build Signum 3.0 firmware
 echo "############################################"
 echo "####    Building Signum 3.0 firmware    ####"
 echo "############################################"
-rm -rf .build
-make clean
-if ! make signum/3_0/elitec:my_keymap;
+make clean >/dev/null 2>&1
+echo "#         Building custom firware          #"
+if ! make signum/3_0/elitec:my_keymap 1>/dev/null;
 then
   exit 1
 fi
-make clean
-if ! make signum/3_0/elitec:default;
+make clean >/dev/null 2>&1
+echo "#         Building default firware         #"
+if ! make signum/3_0/elitec:default 1>/dev/null;
 then
   exit 1
 fi
+echo -e "############################################\n"
 
 # Build Crkbd rev1 firmware
 echo "############################################"
 echo "####     Build Crkbd rev1 firmware      ####"
 echo "############################################"
-rm -rf .build
-make clean
-if ! make crkbd/rev1:my_keymap;
+make clean >/dev/null 2>&1
+echo "#         Building custom firware          #"
+if ! make crkbd/rev1:my_keymap 1>/dev/null;
 then
   exit 2
 fi
-make clean
-if ! make crkbd/rev1:default;
+make clean >/dev/null 2>&1
+echo "#         Building default firware         #"
+if ! make crkbd/rev1:default 1>/dev/null;
 then
   exit 2
 fi
+echo -e "############################################\n"
 
 # Build Iris rev4 firmware
 echo "############################################"
 echo "####      Build Iris rev4 firmware      ####"
 echo "############################################"
-rm -rf .build
-make clean
-if ! make keebio/iris/rev4:my_keymap;
+make clean >/dev/null 2>&1
+echo "#         Building custom firware          #"
+if ! make keebio/iris/rev4:my_keymap 1>/dev/null;
 then
   exit 3
 fi
-make clean
-if ! make keebio/iris/rev4:default;
+make clean >/dev/null 2>&1
+echo "#         Building default firware         #"
+if ! make keebio/iris/rev4:default 1>/dev/null;
 then
   exit 3
 fi
+echo -e "############################################\n"
+
 # Build Quefrency rev1 firmware
 echo "############################################"
-echo "####   Build Quefrency rev1 firmware    ####"
+echo "#      Build Quefrency rev1 firmware       #"
 echo "############################################"
-rm -rf .build
-make clean
-if ! make keebio/quefrency/rev1:my_keymap;
+make clean >/dev/null 2>&1
+echo "#         Building custom firware          #"
+if ! make keebio/quefrency/rev1:my_keymap 1>/dev/null;
 then
   exit 4
 fi
-make clean
-if ! make keebio/quefrency/rev1:default;
+make clean >/dev/null 2>&1
+echo "#         Building default firware         #"
+if ! make keebio/quefrency/rev1:default 1>/dev/null;
 then
   exit 4
 fi
+echo -e "############################################\n"
 
 # Build Dactyl-Manuform firmware
-echo "#############################################"
-echo "####   Build Dactyl-Manuform firmware    ####"
-echo "#############################################"
-rm -rf .build
-make clean
-if ! make handwired/dactyl_manuform/5x6:my_keymap;
+echo "############################################"
+echo "####   Build Dactyl-Manuform firmware   ####"
+echo "############################################"
+make clean >/dev/null 2>&1
+echo "#          Building custom firware         #"
+if ! make handwired/dactyl_manuform/5x6:my_keymap 1>/dev/null;
 then
   exit 5
 fi
-make clean
-if ! make handwired/dactyl_manuform/5x6:default;
+make clean >/dev/null 2>&1
+echo "#          Building default firware        #"
+echo "############################################"
+if ! make handwired/dactyl_manuform/5x6:default 1>/dev/null;
 then
   exit 5
 fi
