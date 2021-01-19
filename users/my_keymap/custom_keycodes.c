0663d385ebf7a9b27fd6f7faa77011be7dc2a56d
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2021-01-19 11:48:52 +0100

    Added redundant code to the users directory

---
+++ users/my_keymap/custom_keycodes.c
@@ -0,0 +1,132 @@
+#include "custom_keycodes.h"
+
+bool is_alt_tab_active = false;
+bool is_cmd_tab_active = false;
+uint16_t alt_tab_timer = 0;
+uint16_t cmd_tab_timer = 0;
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Alt, twice for Alt Gr
+    [TD_LALT_RALT] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case PREV_TAB:
+      if (record->event.pressed) {
+        register_code(KC_LSFT);
+        register_code(KC_LCTRL);
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+        unregister_code(KC_LCTRL);
+        unregister_code(KC_LSFT);
+      }
+      break;
+    case NEXT_TAB:
+      if (record->event.pressed) {
+        register_code(KC_LCTRL);
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+        unregister_code(KC_LCTRL);
+      }
+      break;
+    case OPEN_ALFRED:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+        register_code(KC_SPC);
+      } else {
+        unregister_code(KC_SPC);
+        unregister_code(KC_LGUI);
+      }
+      break;
+    case PREV_WORKSPACE:
+      if (record->event.pressed) {
+        register_code(KC_LCTRL);
+        register_code(KC_LEFT);
+      } else {
+        unregister_code(KC_LEFT);
+        unregister_code(KC_LCTRL);
+      }
+      break;
+    case NEXT_WORKSPACE:
+      if (record->event.pressed) {
+        register_code(KC_LCTRL);
+        register_code(KC_RIGHT);
+      } else {
+        unregister_code(KC_RGHT);
+        unregister_code(KC_LCTRL);
+      }
+      break;
+    case PREV_WINDOW_WIN:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          register_code(KC_LALT);
+        }
+        alt_tab_timer = timer_read();
+        register_code(KC_LSFT);
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+        unregister_code(KC_LSFT);
+      }
+      break;
+    case NEXT_WINDOW_WIN:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          register_code(KC_LALT);
+        }
+        alt_tab_timer = timer_read();
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      break;
+    case PREV_WINDOW_MAC:
+      if (record->event.pressed) {
+        if (!is_cmd_tab_active) {
+          is_cmd_tab_active = true;
+          register_code(KC_LGUI);
+        }
+        cmd_tab_timer = timer_read();
+        register_code(KC_LSFT);
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+        unregister_code(KC_LSFT);
+      }
+      break;
+    case NEXT_WINDOW_MAC:
+      if (record->event.pressed) {
+        if (!is_cmd_tab_active) {
+          is_cmd_tab_active = true;
+          register_code(KC_LGUI);
+        }
+        cmd_tab_timer = timer_read();
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      break;
+  }
+  return true;
+}
+
+void matrix_scan_user(void) {
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
+      unregister_code(KC_LALT);
+      is_alt_tab_active = false;
+    }
+  }
+  if (is_cmd_tab_active) {
+    if (timer_elapsed(cmd_tab_timer) > 1000) {
+      unregister_code(KC_LGUI);
+      is_cmd_tab_active = false;
+    }
+  }
+}
---
+++ users/my_keymap/custom_keycodes.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "quantum.h"
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _WIN,
+    _MAC,
+    _FN,
+    _FN2
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+    PREV_TAB = SAFE_RANGE,
+    NEXT_TAB,
+    OPEN_ALFRED,
+    PREV_WORKSPACE,
+    NEXT_WORKSPACE,
+    PREV_WINDOW_WIN,
+    NEXT_WINDOW_WIN,
+    PREV_WINDOW_MAC,
+    NEXT_WINDOW_MAC
+};
+
+// Defines the keycodes for Tap Dance
+enum tap_dance {
+    TD_LALT_RALT,
+};
