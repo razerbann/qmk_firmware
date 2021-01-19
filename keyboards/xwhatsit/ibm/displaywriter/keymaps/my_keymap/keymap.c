0cb6520f738ca1c2c557775b95b480ed97d8ffc6
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2021-01-19 11:51:11 +0100

    Updated the DisplayWriter 6580 keymap

--- keyboards/xwhatsit/ibm/displaywriter/keymaps/my_keymap/keymap.c
+++ keyboards/xwhatsit/ibm/displaywriter/keymaps/my_keymap/keymap.c
@@ -1,29 +1,31 @@
 #include QMK_KEYBOARD_H
-
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-    _FN,
-    _FN2
-};
+#include "custom_keycodes.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_BASE] = LAYOUT_all(
-        KC_ESC , MO(_FN),                                                                                                                                               KC_INS , KC_HOME, KC_HOME, KC_PGUP,
-        KC_F1  , KC_F2  ,   KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,  KC_BSPC   , KC_DEL,   KC_DEL , KC_END , KC_END , KC_PGDN,
-        KC_F3  , KC_F4  ,    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,    KC_ENT ,
-        KC_F5  , KC_F6  ,     KC_LCTRL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS,                     KC_PGUP, KC_UP  , KC_PGDN, KC_PENT,
-        KC_F7  , KC_F8  ,   KC_LSFT, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,            KC_RSFT  ,           KC_LEFT, KC_DOWN, KC_RGHT, KC_PENT,
-                                          KC_LGUI,                     KC_SPC ,                  KC_LALT
+    [_WIN] = LAYOUT_all(
+        TG(_MAC),  KC_ESC,                                                                                                                                                                KC_INS , KC_HOME, KC_PGUP, TG(_MAC),
+        PREV_TAB, NEXT_TAB,                 KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,  KC_BSPC   , MO(_FN),  KC_DEL , KC_END , KC_PGDN, TG(_MAC),
+        PREV_WINDOW_WIN, NEXT_WINDOW_WIN,    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,    KC_ENT ,
+        _______, _______,                     KC_LCTRL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS,                     KC_PGUP, KC_UP  , KC_PGDN, KC_PENT,
+        _______, _______,                   KC_LSFT, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,            KC_RSFT  ,           KC_LEFT, KC_DOWN, KC_RGHT, KC_PENT,
+                                                          KC_LGUI,                     KC_SPC ,                  TD_LALT_RALT
+    ),
+    [_MAC] = LAYOUT_all(
+        _______, _______,                                                                                                                                                               _______, _______, _______, _______,
+        _______, _______,                   KC_NUBS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______   , _______,  _______, _______, _______, _______,
+        PREV_WINDOW_MAC, NEXT_WINDOW_MAC,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,
+        OPEN_ALFRED, OPEN_ALFRED,             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______,
+        PREV_WORKSPACE, NEXT_WORKSPACE,     _______, KC_GRV , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______  ,           _______, _______, _______, _______,
+                                                          _______,                      _______,                 KC_LALT
     ),
     [_FN] = LAYOUT_all(
         _______, _______,                                                                                                                                               _______, _______, _______, _______,
-        _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______   , _______,  _______, _______, _______, _______,
+        _______, _______,   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 ,  _______   , _______,  _______, _______, _______, _______,
         _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,
         _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______,
         _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______  ,           _______, _______, _______, _______,
-                                          _______,                     MO(_FN2),                 KC_RALT
+                                          _______,                     MO(_FN2),                 _______
     ),
     [_FN2] = LAYOUT_all(
         _______, _______,                                                                                                                                               _______, _______, _______, _______,
