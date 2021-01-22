5fa87012988db52a48c4accb21d7cadd889ba085
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2021-01-22 22:35:32 +0100

    Added condition on name of the keyboard to add the comm protocol to the firmware

--- users/my_keymap/rules.mk
+++ users/my_keymap/rules.mk
@@ -1,17 +1,16 @@
 # Build Options
 #   change yes to no to disable
-
-BOOTMAGIC_ENABLE  = no                      # Virtual DIP switch configuration
-MOUSEKEY_ENABLE   = no                      # Mouse keys
-EXTRAKEY_ENABLE   = no                      # Audio control and System control
-CONSOLE_ENABLE    = no                      # Console for debug
-COMMAND_ENABLE    = no                      # Commands for debug and configuration
+BOOTMAGIC_ENABLE  = no                                # Virtual DIP switch configuration
+MOUSEKEY_ENABLE   = no                                # Mouse keys
+EXTRAKEY_ENABLE   = no                                # Audio control and System control
+CONSOLE_ENABLE    = no                                # Console for debug
+COMMAND_ENABLE    = no                                # Commands for debug and configuration
 
 TAP_DANCE_ENABLE  = yes
 
 SRC += custom_keycodes.c tap_dance.c
 
-ifeq ($(strip $(NOT_IBM_BOARD)), yes)
-    RAW_ENABLE    = yes                     # Enable RAW_HID capabilities
+ifeq (,$(findstring xwhatsit/, $(strip $(KEYBOARD)))) # Add the EnterBootloader communication protocol only if the firmware isn't XWhatsit's
+    RAW_ENABLE    = yes                               # Enable RAW_HID capabilities
     SRC          += comm_protocol.c
 endif
