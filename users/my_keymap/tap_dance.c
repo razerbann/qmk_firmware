5b26efa3b28793a7b56f2e9bdc0287fdb6c1e76e
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2021-01-29 20:36:29 +0100

    Updated several keymaps for adjustments

--- users/my_keymap/tap_dance.c
+++ users/my_keymap/tap_dance.c
@@ -5,6 +5,4 @@
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Alt, twice for Alt Gr
     [LALT_RALT] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT),
-    // Tap once for Ctrl, twice for ESC
-    [LCTRL_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_LCTRL, KC_ESC),
 };
