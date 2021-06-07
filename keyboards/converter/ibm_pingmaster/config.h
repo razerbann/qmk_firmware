6301aa8685b1fa717818a5adcbf4a7ec14f20c9d
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2021-06-07 21:50:55 +0200

    Slight changes to ibm_pingmaster firmware

--- keyboards/converter/ibm_pingmaster/config.h
+++ keyboards/converter/ibm_pingmaster/config.h
@@ -1,19 +1,18 @@
-/*
-Copyright 2021 Abdelkafi Ahmed
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright 2021 Abdelkafi Ahmed
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -21,11 +20,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define PRODUCT_ID   0x4707
 #define DEVICE_VER   0x0001
-#define MANUFACTURER Abdelkafi Ahmed
+#define MANUFACTURER QMK
 #define PRODUCT      IBM 4704 Pingmaster
-
 
 /* matrix size */
 #define MATRIX_ROWS 16  // keycode bit3-6
